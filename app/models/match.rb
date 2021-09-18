056ff2b9c03a9f82c54ca6b9fc50f6471c9d22f7
Jay Reidy <jaylreidy21@gmail.com>
2021-09-18 18:52:35 +1000

    Refactored check_score function to validate score

--- app/models/match.rb
+++ app/models/match.rb
@@ -17,17 +17,17 @@ class Match < ApplicationRecord
 
   def check_score
     reg = /\b(\d{1}|\d{2})-(\d{1}|\d{2})\b/
-    #check if score is a valid format
+    splitScore = score.split("-").map(&:to_i)
     if !reg.match?(score)
       errors.add(:score, "sorry that's an invalid score format, must be two scores seperated by a dash")
       #check if there is a winner and that they have won by more than 2 points
-    elsif score.split('-').map(&:to_i).max < 21
+    elsif splitScore.max < 21
       errors.add(:score, "Match must have a winner")
-    elsif (score.split('-')[0].to_i - score.split('-')[1].to_i) < 2
+    elsif splitScore.inject(:-) < 2
       errors.add(:score, "must be won by at least 2 points")
-    elsif (score.split('-')[0].to_i - score.split('-')[1].to_i) < 2 && score.split('-').map(&:to_i).min >= 21
+    elsif splitScore.inject(:-) == 2 && splitScore.min >= 21
       errors.add(:score, 'can go to into overtime')
-    elsif (score.split('-')[0].to_i - score.split('-')[1].to_i) > 2 && score.split('-').map(&:to_i).max > 21
+    elsif splitScore.inject(:+) > 2 && splitScore.max > 21
       errors.add(:score, 'cannot have a score above 21 unless the score is two points apart')
     end
   end
